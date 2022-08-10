347655f6c73e13a001cbc6b2c7d726ba632777d3
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-08-09 21:17:30 -0300

    Update

--- JavaCollections/src/javacollections/JavaColletions.java
+++ JavaCollections/src/javacollections/JavaColletions.java
@@ -1,21 +1,48 @@
 package javacollections;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+//import java.util.HashSet;
 
 public class JavaColletions {
 
-	
-	
 	public static void main(String[] args) {
-		ArrayList<Integer>myList = new ArrayList<>();
+		HashMap<String, Integer> myBooks= new HashMap<>();
 		
 		
-		int numOfElements = (int) (Math.random() * 100);
+		myBooks.put("Ender's Game", 4);
+		myBooks.put("Ringworld", 3);
+		myBooks.put("Heir to the Empire", 5);
 		
-		for (int i = 0; i < numOfElements; i++)
-			myList.add(i);
 		
-		for (int element : myList)
-		System.out.println(element);
+		//for(String title : myBooks.keySet())
+			//System.out.println(title);
+		
+		//for(int score : myBooks.values())
+			//System.out.println(score);
+		
+		for(String title :myBooks.keySet())
+			System.out.println(title + " é nota " + myBooks.get(title));
+		
 	}
-}
+	
+	
+	
+	//public static void main(String[] args) {
+		//HashSet<String> mySet = new HashSet<>();
+		
+		
+		//mySet.add("Banana");
+		//mySet.add("Apple");
+		//mySet.add("Orange");
+		
+		//for(String fruit : mySet)
+		//*	System.out.println(fruit);
+		
+		
+		//System.out.println("Banana: " +mySet.contains("Banana")); //True
+		
+		//mySet.remove("Banana");
+		
+		//*System.out.println("Banana: " +mySet.contains("Banana")); // False
+	}
+//}
